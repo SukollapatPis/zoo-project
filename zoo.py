bf8aea72971ed39b4c714fa693ea122dd6de070e
SukollapatPis <126540619+SukollapatPis@users.noreply.github.com>
2024-11-11 14:57:50 +0700

    Update zoo.py

--- zoo.py
+++ zoo.py
@@ -1,10 +1,16 @@
+# Lab 15
+# Sukollapat Pisuchpen
+# 6687052
+# Sec 2
 class Zoo:
     def get_ticket_price(self, age):
-        if 0 < age <= 12:
+        if age < 0: # Path
+            return "error, Please enter the correct age"
+        elif age >= 0 and age <= 12:
             return 50
-        elif 13 <= age < 20:
+        elif age >= 13 and age <= 20:
             return 100
-        elif 21 < age <= 60:
+        elif age >= 21 and age <= 60:
             return 150
-        elif age >= 60:
+        else:
             return 100
